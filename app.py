31e8964096ae8e25833097c351d254c00c38b54f
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-04-13 22:52:33 +0000

    slushies tutorial completion

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def index():
     user_name = "Mustafa"
     return render_template("index.html", user_name=user_name)
 
+@app.route('/testing')
+def name():
+    user_name = "Testing"
+    return render_template("testing.html", user_name=user_name)
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
