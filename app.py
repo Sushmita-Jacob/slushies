ac7e6bcf57a0a8f1426d6376e99dfcaef5ac35e4
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-04-13 22:22:08 +0000

    Testing out structure ("Hello World")

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    user_name = "Mustafa"
+    return render_template("index.html", user_name=user_name)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
