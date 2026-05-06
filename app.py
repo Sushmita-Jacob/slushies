5ba68cbcb2a61ab9859cb20b5abced69a45ee5ba
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-05-06 23:40:01 +0000

    trying to request openweathermap data using an api

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def index():
+    if request.method == "POST":
+        city = request.form['city']
+        country = request.form['country']
+        api_key = "###"
+        weather_url = requests.get(f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city},{country}&units=imperial')
+        weather_data = weather_url.json()
+        temp = round(weather_data['main']['temp'])
+        humidity = weather_data['main']['humidity']
+        wind_speed = weather_data['wind']['speed']
+        return render_template("result.html", temp=temp, humdiity=humidity, wind_speed=wind_speed, city=city)
     user_name = "Mustafa"
     return render_template("index.html", user_name=user_name)
 
@@ -14,3 +26,13 @@ def name():
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
+
+city="Indianapolis"
+country = "US"
+api_key = "###"
+weather_url = requests.get(f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city},{country}&units=imperial')
+weather_data = weather_url.json()
+temp = round(weather_data['main']['_temp_'])
+humidity = weather_data['main']['humidity']
+wind_speed = weather_data['wind']['speed']
