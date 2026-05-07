e4b5a1aa6726bf90e0f56fb92a3f38825618398c
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-05-07 20:56:29 +0000

    attempt to integrate api

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     if request.method == "POST":
         city = request.form['city']
         country = request.form['country']
-        api_key = "###"
+        api_key = "edc9763b43e65662faa4ed8a50cf6537"
         weather_url = requests.get(f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city},{country}&units=imperial')
         weather_data = weather_url.json()
         temp = round(weather_data['main']['temp'])
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import requests
 import json
 city = "Indianapolis"
 country = "US"
-api_key = "###"
+api_key = "edc9763b43e65662faa4ed8a50cf6537"
 weather_url = requests.get(f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city},{country}&units=imperial')
 weather_data = weather_url.json()
+temp = weather_data['main']['temp']
